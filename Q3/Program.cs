8d41de50581c075a41c733d3fde709f955f50109
Adamwalshe <123950051+Adamwalshe@users.noreply.github.com>
2023-04-04 19:23:24 +0100

    Add files via upload

---
+++ Q3/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintTriangle();
+            PrintTriangleUpsideDown();
+            PrintTriangleGeneral();
+        }
+        static private void PrintTriangle()
+        {
+            
+            for (int i = 0; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+        }
+        static private void PrintTriangleUpsideDown()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 1; j<(6-i); j++ )
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+
+        }
+        static private void PrintTriangleGeneral()
+        {
+            Console.WriteLine("How many rows in your triangle:");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = 0; j < (1+i); j++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
